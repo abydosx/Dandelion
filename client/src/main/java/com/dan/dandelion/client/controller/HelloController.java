edcb3465d8133472b3c8b6fe106bf9b5d023af0c
mata <mrwangshibo@163.com>
2022-04-30 23:32:15 +0800

    updatesql

--- client/src/main/java/com/dan/dandelion/client/controller/HelloController.java
+++ client/src/main/java/com/dan/dandelion/client/controller/HelloController.java
@@ -12,3 +12,5 @@ public class HelloController {
 
 
 }
+
+public class aa (){}
--- client/src/main/resources/db/mini_artical.sql
+++ client/src/main/resources/db/mini_artical.sql
@@ -1,9 +1,8 @@
-DROP TABLE IF EXISTS `mini_artical`;
 CREATE TABLE `mini_artical` (
-  `uni_id` int(10) unsigned zerofill NOT NULL,
+  `uni_id` int(10) unsigned zerofill NOT NULL AUTO_INCREMENT,
   `content` text CHARACTER SET big5 COLLATE big5_chinese_ci,
   `user_id` int DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`uni_id`)
-) ENGINE=InndDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
